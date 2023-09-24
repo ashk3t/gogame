0b770f9acb1499ce409ad508b36602410e039c9a
ashk3t <daniildudov@gmail.com>
2023-09-24 14:47:40 +0300

    board styles proper resizing

--- frontend/src/components/Board.tsx
+++ frontend/src/components/Board.tsx
@@ -1,17 +1,35 @@
 import styles from "../styles/Board.module.css"
-import {useEffect, useState} from "react"
+import {useEffect, useMemo, useState} from "react"
 import {useActions, useAppDispatch, useAppSelector} from "../hooks/redux"
 import {fetchAllGames} from "../action-creators/game"
 import CircleSvg from "../assets/CircleSvg"
+import {BoardIntersectionStyler} from "../utils"
 
 export default function Board() {
-  const board = Array.from({length: 19}, () => new Array(19).fill(0))
+  const xSize = 15
+  const ySize = 6
+  const board = Array.from({length: xSize}, () => new Array(ySize).fill(0))
+
+  const [intersectionStyler] = useState(new BoardIntersectionStyler(xSize, ySize))
+
+  function getIntersectionBackground(x: number, y: number) {
+    return useMemo(() => intersectionStyler.getBackground(x, y), [x, y])
+  }
 
   return (
-    <div className={styles.board}>
-      {board.map((row: any[]) =>
-        row.map((cell: number) => (
-          <div className={styles.boardCell}>
+    <div
+      className={styles.board}
+      style={{
+        gridTemplateRows: `repeat(${ySize}, 1fr)`,
+      }}
+    >
+      {board.map((column: any[], x: number) =>
+        column.map((intersection: number, y: number) => (
+          <div
+            key={`(${x}, ${y})`}
+            className={styles.intersection}
+            style={getIntersectionBackground(x, y)}
+          >
             <CircleSvg className={styles.stone} />
           </div>
         )),
--- frontend/src/utils.ts
+++ frontend/src/utils.ts
@@ -1,7 +1,6 @@
 import {capitalize} from "lodash"
 import {nicknameLabels} from "./consts/utils"
 
-// Utils
 export function loadScript(src: string) {
   return new Promise<void>((resolve, reject) => {
     if (document.querySelector(`script[src="${src}"]`)) return resolve()
@@ -29,5 +28,43 @@ export function getRandomNicknameLabel(): string {
 }
 
 export function startsWithVowel(word: string): boolean {
-  return ['a', 'e', 'i', 'o', 'u'].indexOf(word[0].toLowerCase()) !== -1
+  return ["a", "e", "i", "o", "u"].indexOf(word[0].toLowerCase()) !== -1
+}
+
+export class BoardIntersectionStyler {
+  xSize: number
+  ySize: number
+
+  constructor(xSize: number, ySize: number) {
+    console.log("NICE")
+    this.xSize = xSize
+    this.ySize = ySize
+  }
+
+  getBackground(x: number, y: number) {
+    const fg = "var(--text)"
+    const bg = "var(--base)"
+    const w = 3
+    let hColors = `${fg}, ${fg}`
+    let vColors = `${fg}, ${fg}`
+
+    if (x == 0) {
+      hColors = `${bg} ${50 - w}%, ${fg} ${50 - w}%`
+    } else if (x + 1 == this.xSize) {
+      hColors = `${fg} ${50 + w}%, ${bg} ${50 + w}%`
+    }
+    if (y == 0) {
+      vColors = `${bg} ${50 - w}%, ${fg} ${50 - w}%`
+    } else if (y + 1 == this.ySize) {
+      vColors = `${fg} ${50 + w}%, ${bg} ${50 + w}%`
+    }
+
+    console.log(x, y)
+    return {
+      background: `
+        linear-gradient(to bottom, ${vColors}) no-repeat center/${2 * w}% 100%,
+        linear-gradient(to right,  ${hColors}) no-repeat center/100% ${2 * w}%
+      `,
+    }
+  }
 }
