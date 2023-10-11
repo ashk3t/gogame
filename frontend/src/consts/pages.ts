f0f9b8c2902e5829ef0202b4a87eac5816acced8
shk3t <daniildudov@gmail.com>
2023-10-11 10:49:29 +0300

    game page mobile ui fix

--- frontend/src/consts/pages.ts
+++ frontend/src/consts/pages.ts
@@ -2,15 +2,17 @@ import GameListPage from "../pages/GameListPage"
 import GamePage from "../pages/GamePage"
 import StartPage from "../pages/StartPage"
 
-export const GAME_LIST_PATH = "/game_list"
 export const START_PATH = "/start"
+export const SEARCH_PATH = "/search"
+export const GAME_LIST_PATH = "/game_list"
 export const GAME_PATH = "/game"
 
 export const defaultRoutes = [
-  {path: GAME_LIST_PATH, Component: GameListPage, title: "game_list/"},
-  {path: START_PATH, Component: StartPage, title: "start/"},
+  {path: SEARCH_PATH, Component: StartPage},
+  {path: START_PATH, Component: StartPage},
+  {path: GAME_LIST_PATH, Component: GameListPage},
 ]
 
 export const inGameRoutes = [
-  {path: GAME_PATH, Component: GamePage, title: "game/"},
+  {path: GAME_PATH, Component: GamePage},
 ]
--- frontend/src/pages/SearchPage.tsx
+++ frontend/src/pages/SearchPage.tsx
@@ -1,24 +1,8 @@
-import {useEffect, useState} from "react"
-import {useActions, useAppDispatch, useAppSelector} from "../hooks/redux"
-import {fetchAllGames} from "../action-creators/game"
-
 export default function SearchPage() {
-  // const {games} = useAppSelector((state) => state.gameReducer)
-  const games: any[] = []
-  const {fetchAllGames} = useActions()
-
-  useEffect(() => {
-    fetchAllGames()
-  }, [])
 
   return (
     <main>
-      <h3>This page in development:</h3>
-      <div>
-        {games.map((game: any) => (
-          <div>{game.toString()}</div>
-        ))}
-      </div>
+      <h3>Search...</h3>
     </main>
   )
 }
