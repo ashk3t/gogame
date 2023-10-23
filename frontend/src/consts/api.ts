30d2f5600fe94a432b9528373ee4e9b04c75aa25
shk3t <daniildudov@gmail.com>
2023-10-23 05:19:31 +0300

    api path prefix (frontend)

--- frontend/src/consts/api.ts
+++ frontend/src/consts/api.ts
@@ -1,4 +1,5 @@
-export const API_URL = "http://localhost:8000"
-export const WS_API_URL = "ws://localhost:8000"
+const API_PATH_PREFIX = import.meta.env.MODE == "production" ? "/gogame/api" : ""
+export const API_URL = "http://localhost:8000" + API_PATH_PREFIX
+export const WS_API_URL = "ws://localhost:8000" + API_PATH_PREFIX
 
 export const DEFAULT_PAGE_SIZE = 18
