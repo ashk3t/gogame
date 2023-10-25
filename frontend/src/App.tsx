8722ff64f9299f5b8c7351d2d677efb4e023a935
shk3t <daniildudov@gmail.com>
2023-10-25 06:30:40 +0300

    rename

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import {BrowserRouter} from "react-router-dom"
 import AppRouter from "./components/AppRouter"
-import RouterStateManager from "./components/StateManager"
+import RouterStateManager from "./components/RouterStateManager"
 // import "index.css"
 
 export default function App() {
